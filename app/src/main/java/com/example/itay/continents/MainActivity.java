e7e621aff984a2ac2c1fb186dfb64db3de8b090c
Itay <aa0801@bs.amalnet.k12.il>
2019-10-16 13:57:15 +0300

    beta

--- app/src/main/java/com/example/itay/continents/MainActivity.java
+++ app/src/main/java/com/example/itay/continents/MainActivity.java
@@ -9,11 +9,23 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+/**
+ * @author Itay Elbaz
+ * @version beta
+ * @since 16/10/19
+ * This activity is the main activity, where you can choose a continent out of 6 options and then click next.
+ * The option you chose will be printed as a toast.
+ */
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     ListView lv;
     String[] str={"Africa","Europe","Asia","South America","North America"};
     int x=60;
+    /**
+     * lv is the java list view widget
+     * The array str contains the continents' names
+     * x is an index for of the chosen continent
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,17 +42,30 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         lv.setAdapter(adp);
     }
 
+    /**
+     *This function starts when a continent is chosen
+     * @param position-index of the place of the chosen item in the ListView.
+     * @return the function prints the continent name as a toast.
+     */
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         Toast.makeText(this, ""+str[position], Toast.LENGTH_SHORT).show();
         x=position;
     }
 
+    /**
+     * This function starts when the button "next" is clicked.
+     * if a continent is chosen we move to the next activity-with the continent's index in str.
+     * else, we don't move to the next activity and a toast comes-"please choose a continent"
+     */
     public void btn(View view) {
         if(x!=60) {
-            Intent si=new Intent(this,Main2Activity.class);
+            Intent si=new Intent(this,countries.class);
             si.putExtra("position",x);
             startActivity(si);
+        }
+         else {
+            Toast.makeText(this, "please choose a continent", Toast.LENGTH_SHORT).show();
         }
     }
 }
--- app/src/main/java/com/example/itay/continents/countries.java
+++ app/src/main/java/com/example/itay/continents/countries.java
@@ -9,7 +9,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class Main2Activity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+public class countries extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
 
     TextView tv,tv2;
@@ -17,37 +17,54 @@ public class Main2Activity extends AppCompatActivity implements AdapterView.OnIt
     String[][] m={{"Egypt","South Africa","Tanzania","Sudan","Algeria","Ethiopia","Nigeria"},{"Greece","Germany","Turkey","France","United Kingdom","Italy","Spain"},{"israel","China","India","Nepal","Saudi Arabia","Yemen","Indonesia"},{"Argentina","Bolivia","Brazil","Chile","Colombia","Ecuador","Peru"},{"Bahamas","Barbados","Canada","Cuba","Costa Rica","El Salvador","United States of America"}};
     String[][] m2={{"Cairo","Cape Town","Dodoma","Khartoum","Algiers","Addis Ababa","Abuja","Athens","Berlin","Ankara","Paris","London","Rome","Madrid","Jerusalem","Beijing","New Delhi","Kathmandu","Riyadh","Sana'a","Jakarta","Buenos Aires","La Paz","Brasilia","Santiago","Bogotá","Quito","Lima","Nassau","Bridgetown","Ottawa","Havana","San Jose","San Salvador","Washington, D.C."},
             {"98,002,045","58,558,270","51,046,000","40,235,000","40,100,000","112,078,730","200,963,599","10,768,193","82,887,000","82,003,882","67,372,000","66,040,229","60,390,560","46,733,038","8,372,000","1,387,160,730","1,324,009,090","28,038,000","26,849,000","26,745,000","255,462,000","43,132,000","10,520,000","204,519,000","18,006,000","45,549,000","16,279,000","31,153,000","422,678","381,320","37,279,800","11,221,060"," 5,003,393","6,643,359"," 327,929,000"}};
-
-
     int x;
+    /**
+     * lv is the java list view widget
+     * tv and tv2 are the java text view widgets
+     * The matrix m contains the countries' names
+     * The matrix mm contains the countries' capitals' names and their populations.
+     * x is an index for of the chosen country.
+     */
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.countries);
 
         lv=(ListView) findViewById(R.id.lv);
         tv=(TextView) findViewById(R.id.tv);
         tv2=(TextView) findViewById(R.id.tv2);
 
         Intent gi=getIntent();
-        x=gi.getIntExtra("position",2);
+        x=gi.getIntExtra("position",-1);
 
         ArrayAdapter<String> adp=new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,m[x]);
+
 
         lv.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
         lv.setOnItemClickListener(this);
         lv.setAdapter(adp);
     }
 
+    /**
+     *This function starts when a continent is chosen
+     * @param position-index of the place of the chosen item in the ListView.
+     * @return the function prints the continent name as a toast.
+     */
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
         tv.setText("Capital City: "+m2[0][x*7+position]);
         tv2.setText("Population: " + m2[1][x*7+position]);
     }
 
-
+    /**
+     * This function starts when the button "back" is clicked
+     * the function finishes the activity and we move back to the previous activity.
+     */
     public void btn2(View view) {
+
         finish();
     }
 }
